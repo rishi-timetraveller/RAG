fd5f30028630c1890c1522d69c07a15994c7882f
Rishi Shrivastav <145578443+rishi-timetraveller@users.noreply.github.com>
2024-06-10 01:32:09 +0530

    Update chatpdf1.py

--- chatpdf1.py
+++ chatpdf1.py
@@ -45,7 +45,7 @@ def get_conversational_chain():
 
     prompt_template = """
     Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
-    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
+    provided context, then search t on web  just say, "answer is not available in the context here is the web answer", don't provide the wrong answer\n\n
     Context:\n {context}?\n
     Question: \n{question}\n
 
